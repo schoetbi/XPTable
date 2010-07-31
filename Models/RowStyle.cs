3f8ba613cfb02f9f989d694ed4dcdbd8d22a4862
adarmus <adarmus@907c5300-f82f-0410-99ae-ed938deb68d4>
2010-07-31 19:12:51 +0000

    Fixed  bad edit in RowStyle
    git-svn-id: https://xptable.svn.sourceforge.net/svnroot/xptable/trunk@182 907c5300-f82f-0410-99ae-ed938deb68d4

--- Models/RowStyle.cs
+++ Models/RowStyle.cs
@@ -30,98 +30,92 @@ using System.Drawing;
 
 namespace XPTable.Models
 {
-	/// <summary>
-	/// Stores visual appearance related properties for a Row
-	/// </summary>
-	public class RowStyle
-	{
-		#region Class Data
-		/// <summary>
-		/// The background color of the Row
-		/// </summary>
-		private Color backColor;
+    /// <summary>
+    /// Stores visual appearance related properties for a Row
+    /// </summary>
+    public class RowStyle
+    {
+        #region Class Data
+        /// <summary>
+        /// The background color of the Row
+        /// </summary>
+        private Color backColor;
 
-		/// <summary>
-		/// The foreground color of the Row
-		/// </summary>
-		private Color foreColor;
+        /// <summary>
+        /// The foreground color of the Row
+        /// </summary>
+        private Color foreColor;
 
-		/// <summary>
-		/// The font used to draw the text in the Row
-		/// </summary>
-		private Font font;
+        /// <summary>
+        /// The font used to draw the text in the Row
+        /// </summary>
+        private Font font;
 
-		/// <summary>
-		/// The alignment of the text in the Row
-		/// </summary>
-		private RowAlignment alignment;
+        /// <summary>
+        /// The alignment of the text in the Row
+        /// </summary>
+        private RowAlignment alignment;
 
-		#endregion
+        #endregion
 
-		#region Constructor
-		/// <summary>
-		/// Initializes a new instance of the RowStyle class with default settings
-		/// </summary>
-		public RowStyle()
-		{
-			this.backColor = Color.Empty;
-			this.foreColor = Color.Empty;
-			this.font = null;
-			this.alignment = RowAlignment.Center;
-		}
-		#endregion
+        #region Constructor
+        /// <summary>
+        /// Initializes a new instance of the RowStyle class with default settings
+        /// </summary>
+        public RowStyle()
+        {
+            this.backColor = Color.Empty;
+            this.foreColor = Color.Empty;
+            this.font = null;
+            this.alignment = RowAlignment.Center;
+        }
+        #endregion
 
-		#region Properties
-		/// <summary>
-		/// Gets or sets the Font used by the Row
-		/// </summary>
-		[Category("Appearance"),
-		Description("The font used to display text in the row")]
-		public Font Font
-		{
-			get { return this.font; }
-			set { this.font = value; }
-		}
+        #region Properties
+        /// <summary>
+        /// Gets or sets the Font used by the Row
+        /// </summary>
+        [Category("Appearance"),
+        Description("The font used to display text in the row")]
+        public Font Font
+        {
+            get { return this.font; }
+            set { this.font = value; }
+        }
 
-		/// <summary>
-		/// Gets or sets the background color for the Row
-		/// </summary>
-		[Category("Appearance"),
-		Description("The background color used to display text and graphics in the row")]
-		public Color BackColor
-		{
-			get { return this.backColor; }
-			set { this.backColor = value; }
-		}
+        /// <summary>
+        /// Gets or sets the background color for the Row
+        /// </summary>
+        [Category("Appearance"),
+        Description("The background color used to display text and graphics in the row")]
+        public Color BackColor
+        {
+            get { return this.backColor; }
+            set { this.backColor = value; }
+        }
 
-		/// <summary>
-		/// Gets or sets the foreground color for the Row
-		/// </summary>
-		[Category("Appearance"),
-		Description("The foreground color used to display text and graphics in the row")]
-		public Color ForeColor
-		{
-			get { return this.foreColor; }
-			set { this.foreColor = value; }
-		}
+        /// <summary>
+        /// Gets or sets the foreground color for the Row
+        /// </summary>
+        [Category("Appearance"),
+        Description("The foreground color used to display text and graphics in the row")]
+        public Color ForeColor
+        {
+            get { return this.foreColor; }
+            set { this.foreColor = value; }
+        }
 
-		/// <summary>
-		/// Gets or sets the vertical alignment of the text displayed in the Row
-		/// </summary>
-		[Category("Appearance"),
-		DefaultValue(RowAlignment.Center),
-		Description("The vertical alignment of the objects displayed in the row")]
-		public RowAlignment Alignment
-		{
-			get { return this.alignment; }
-			set { this.alignment = value; }
-		}
-
-			set
-			{
-				this.alignment = value;
-			}
-		}
-		#endregion
-	}
+        /// <summary>
+        /// Gets or sets the vertical alignment of the text displayed in the Row
+        /// </summary>
+        [Category("Appearance"),
+        DefaultValue(RowAlignment.Center),
+        Description("The vertical alignment of the objects displayed in the row")]
+        public RowAlignment Alignment
+        {
+            get { return this.alignment; }
+            set { this.alignment = value; }
+        }
+        #endregion
+    }
 }
