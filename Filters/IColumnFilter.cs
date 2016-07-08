f537ab5259c638ba21b7a8dafd0032c50fd7e16b
adarmus <adam@blairfamily.co.uk>
2016-07-08 16:01:03 +0100

    Added comments

--- Filters/IColumnFilter.cs
+++ Filters/IColumnFilter.cs
@@ -6,10 +6,22 @@ using XPTable.Models;
 
 namespace XPTable.Filters
 {
+    /// <summary>
+    /// Implemented by classes that define filtering behaviour for a column
+    /// </summary>
     public interface IColumnFilter
     {
+        /// <summary>
+        /// Called to determine whether this cell can be shown by this filter
+        /// </summary>
+        /// <param name="cell"></param>
+        /// <returns></returns>
         bool CanShow(Cell cell);
 
+        /// <summary>
+        /// Called when the filter button is clicked on this column
+        /// </summary>
+        /// <param name="e"></param>
         void OnHeaderFilterClick(HeaderMouseEventArgs e);
     }
 }
