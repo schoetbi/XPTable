7bf2616378fcbabbeb36059016741dcb9fc346ed
schoetbi <schoetbi@users.noreply.github.com>
2024-04-22 14:17:09 +0200

    Integrated gitversion to create assemblyversion

---
+++ build_nuget.ps1
@@ -0,0 +1,7 @@
+# delete bin and obj folder using powershell
+Remove-Item -Recurse -Force src/XPTable/bin
+Remove-Item -Recurse -Force src/XPTable/obj
+
+dotnet build XPTable.sln --configuration=release
+dotnet pack src/XPTable/XPTable.csproj
+
